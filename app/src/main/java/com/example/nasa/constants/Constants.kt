88db3aa773ef532a13e5569fd2ac48b0769396a6
Aly <alytaha46@gmail.com>
2022-11-30 19:22:19 +0200

    Done Room API and Repository and remove api key last update

--- app/src/main/java/com/example/nasa/constants/Constants.kt
+++ app/src/main/java/com/example/nasa/constants/Constants.kt
@@ -1,9 +1,11 @@
 package com.example.nasa.constants
 
+import com.example.nasa.BuildConfig
+
 object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = ""
+    const val API_KEY = BuildConfig.API_KEY
 
 }
